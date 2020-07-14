9b26f5e8ad1b3fc4e8db8698cc82037a1b1282a8
杨志雄 <1802471011@qq.com>
2020-07-14 15:59:07 +0800

    1

---
+++ src/test/java/com/example/equipment/EquipmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.equipment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
