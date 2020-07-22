54693500974740c4db9c1ec34384acd3dff0fca1
Xiang <1014599072@qq.com>
2020-07-22 18:55:11 +0800

    111

--- src/main/java/com/example/equipment/controller/OutputequipmentController.java
+++ src/main/java/com/example/equipment/controller/OutputequipmentController.java
@@ -1,9 +1,15 @@
 package com.example.equipment.controller;
 
 
+import com.example.equipment.entity.Outputequipment;
+import com.example.equipment.service.OutputequipmentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * <p>
@@ -14,8 +20,19 @@ import org.springframework.stereotype.Controller;
  * @since 2020-07-14
  */
 @Controller
-@RequestMapping("/generator/outputequipment")
+@RequestMapping("view/generator/outputequipment")
+@ResponseBody
 public class OutputequipmentController {
+    @Autowired
+    OutputequipmentService service;
+    public List<Outputequipment> queryAll(){
+
+       return service.queryAll();
+    }
+    public List<Outputequipment> queryByName(Outputequipment o){return service.queryByName(o);}
+    public Integer insert(Outputequipment o){return service.insert(o);}
+    public Integer updateByName(Outputequipment o){return service.updateByName(o);}
+    public Integer deleteByName(Outputequipment o){return service.deleteByName(o);}
 
 }
 
--- src/main/java/com/example/equipment/service/impl/SeriaportinputequipmentServiceImpl.java
+++ src/main/java/com/example/equipment/service/impl/SeriaportinputequipmentServiceImpl.java
@@ -1,10 +1,14 @@
 package com.example.equipment.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.example.equipment.entity.Seriaportinputequipment;
 import com.example.equipment.mapper.SeriaportinputequipmentMapper;
 import com.example.equipment.service.SeriaportinputequipmentService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +20,33 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class SeriaportinputequipmentServiceImpl extends ServiceImpl<SeriaportinputequipmentMapper, Seriaportinputequipment> implements SeriaportinputequipmentService {
+    @Autowired
+    SeriaportinputequipmentMapper mapper;
 
+    @Override
+    public List<Seriaportinputequipment> queryAll() {
+        return mapper.selectList(null);
+    }
+
+    @Override
+    public List<Seriaportinputequipment> queryByName(Seriaportinputequipment o) {
+        QueryWrapper queryWrapper =new QueryWrapper();
+        queryWrapper.eq("name",o.getName());
+        return mapper.selectList(queryWrapper);
+    }
+
+    @Override
+    public Integer insert(Seriaportinputequipment o) {
+        return mapper.insert(o);
+    }
+
+    @Override
+    public Integer updateByName(Seriaportinputequipment o) {
+        return mapper.updateById(o);
+    }
+
+    @Override
+    public Integer deleteByName(Seriaportinputequipment o) {
+        return mapper.deleteById(o);
+    }
 }
